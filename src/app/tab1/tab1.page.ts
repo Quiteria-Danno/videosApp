ce49f42da9b230c38f58dfdd9a79a0ee6c9da195
quiteria.danno01@etec.sp.gov.br <quiteria_danno@hotmail.com>
2021-05-29 00:29:17 -0300

    Adicionado array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -9,8 +10,28 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController)  {}
+  titulo = 'Videos';
 
+  listaVideos: IFilme [] = [
+    {
+      nome: '22 Contra a Terra (2021)',
+      lancamento: '30/04/2021',
+      duracao: '5m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yXhjueyyNNrGinwM9WIxuwNqzbm.jpg',
+      generos: ['Família', 'Animação', 'Comédia']
+    },
+    {
+      nome: 'Pets Monstruosos (2021)',
+      lancamento: '02/04/2021',
+      duracao: '6m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/dkokENeY5Ka30BFgWAqk14mbnGs.jpg',
+      generos: ['Família', 'Animação', 'Comédia']
+    }
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController)  {}
     async exibirAlertaFavorito() {
       const alert = await this.alertController.create({
         header: 'Alerta!',
