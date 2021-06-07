4bca01f8f7f807d5cd86cd57f396cf057606cf68
quiteria.danno01@etec.sp.gov.br <quiteria_danno@hotmail.com>
2021-06-07 12:56:31 -0300

    Ajuste até aula 30 Ajuste da data e Lista de Genero

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,11 @@
+import { GeneroService } from './../services/genero.service';
+import { IListaFilmes, IFilmeApi } from './../models/IFilmeAPImodel';
+import { FilmeService } from './../services/filme.service';
 import { DadosService } from './../services/dados.service';
-import { Component } from '@angular/core';
+import { IFilme } from '../models/IFilme.model';
+import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
-import { IFilme } from '../models/IFilme.model';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,9 +13,9 @@ import { Router } from '@angular/router';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  titulo = 'Videos';
+  titulo = 'Filmes';
 
   listaVideos: IFilme [] = [
     {
@@ -81,16 +84,34 @@ export class Tab1Page {
     }
   ];
 
+  listaFilmes: IListaFilmes;
+
+  generos: string[] = [];
+
+
   constructor(
     public alertController: AlertController,
     public toastController: ToastController,
     public dadosService: DadosService,
+    public filmeService: FilmeService,
+    public generoService: GeneroService,
     public route: Router){}
 
-    exibirFilme(filme: IFilme){
-      this.dadosService.guardarDados('filme', filme);
-      this.route.navigateByUrl('/dados-filme');
+  buscarFilmes(evento: any) {
+    console.log(evento.target.value);
+    const busca = evento.target.value;
+    if(busca && busca.trim() !== ''){
+      this.filmeService.buscarFilmes(busca).subscribe(dados =>{
+        console.log(dados);
+        this.listaFilmes = dados;
+      });
     }
+  }
+
+  exibirFilme(filme: IFilmeApi){
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -124,4 +145,13 @@ export class Tab1Page {
     toast.present();
   }
 
+  ngOnInit(){
+
+    this.generoService.buscarGeneros().subscribe(dados =>{
+      console.log('Generos:',dados.genres);
+      dados.genres.forEach(genero => {
+        this.generos[genero.id] = genero.name;
+      });
+    });
+  }
 }
